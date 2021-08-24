5e33142d0a0a8c13a4401948df1981a029109fd3
Anderson Bassan <bassan.programador@gmail.com>
2021-08-24 16:50:15 -0300

    Bug fix: fix search button not being selected

--- main.js
+++ main.js
@@ -18,7 +18,7 @@
 
 
 /* sets the debug mode */
-let debug = false;
+let debug = true;
 
 /* informs the developer about the extension state */
 console.log(debug? 'Extension loaded sucessfuly' : '');
@@ -36,14 +36,14 @@ console.log(debug? 'Extension loaded sucessfuly' : '');
 
 /* == KEYDOWN EVENTLISTENERS == */
 
+
 document.addEventListener('keydown', (event) => {
 
   /* gets the key script elements */
   let searchbox = document.querySelector('input[placeholder=\'Buscar na ordem\']');
   let checkbox = document.querySelector('.x-grid-row-checker');
-  let search_button = document.querySelector('div.x-panel.x-border-item.x-panel-app-main div div #Suite-view-CrudMaintOrders-body div[style=\'right: auto; left: 329px; top: 5px; margin: 0px; height: 632px; width: 781px;\'] div div div .x-btn-inner.x-btn-inner-center');
   let print_menu; /* ATM I have no idea what element is precisely the menu and how to open the print menu, instead of just straight up printing */
-
+  let search_button = document.querySelector('div.x-panel.x-border-item.x-panel-app-main div div #Suite-view-CrudMaintOrders-body div[style=\'right: auto; left: 329px; top: 5px; margin: 0px; height: 632px; width: 781px;\'] div div div .x-btn-inner.x-btn-inner-center');
 
   /* handles the different key pressed scenarious */
   if (event.ctrlKey && event.key == 'S') {
@@ -54,11 +54,16 @@ document.addEventListener('keydown', (event) => {
 
     /* injects the clipboard (copied text) into the searchbox and then searches for the Maintaince Order */
     window.navigator.clipboard.readText().then(clipboard_text => {
-        console.log((debug)? ('Value: ', searchbox.value) : '');
+
+	if (search_button == null) {
+	  search_button = document.querySelector('div.x-panel.x-border-item.x-panel-app-main div div #Suite-view-CrudMaintOrders-body div[style=\'right: auto; left: 329px; top: 5px; margin: 0px; height: 632px; width: 781px;\'] div div div .x-btn-inner.x-btn-inner-center');
+	}
 
         searchbox.value = clipboard_text;
         searchbox.select();
-        search_button.click();
+	if (search_button) {
+	  search_button.click();
+	}
     });
   
   }
